12feb41721a29f671420bd9e47e4402620062101
Akbar <akbarbahodirov2007@email.com>
2026-03-31 10:30:19 +0500

    fix React import

--- src/js/AdminPanel.jsx
+++ src/js/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect, React } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../css/AdminPanel.module.css";
 import { fetchUsers, fetchAdminStats, fetchUserClients, createUser, deleteUser, makeAdmin, removeAdmin, makeMaster, removeUser, getUser } from "../api";
